c6e8c6088c4717d102508c81b41c19ffe17e4101
Jakub Rutrle <rutrle@seznam.cz>
2025-01-08 01:24:42 +0100

    Populate navbar

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,27 @@ import "./styles.css";
 console.log("index")
 console.log("webpack live server")
 
-class header
 
 const body = document.querySelector("body")
 const header = document.createElement("header")
 const nav = document.createElement("nav")
 
+const nav_button_home = document.createElement("button")
+nav_button_home.textContent = "Home"
+
+const nav_button_menu = document.createElement("button")
+nav_button_menu.textContent = "Menu"
+
+const nav_button_about = document.createElement("button")
+nav_button_about.textContent = "About"
+
+
+const content = document.createElement("div")
+content.id = "content"
 
 body.appendChild(header)
+body.appendChild(content)
 header.appendChild(nav)
+nav.appendChild(nav_button_home)
+nav.appendChild(nav_button_menu)
+nav.appendChild(nav_button_about)
